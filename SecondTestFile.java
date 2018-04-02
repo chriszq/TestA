f62c9010d11eba30084e2c4603e7b6f955483d47
chriszq <cchhristopher@gmail.com>
2018-04-02 22:41:04 +0100

    deleting 1 file

--- SecondTestFile.java
+++
@@ -1,9 +0,0 @@
-public class SecondTestFile
-{
-	public static void main(String[] args)
-	{
-		System.out.println("second file");
-		System.out.println("adding an extra line here");
-	}
-}
-//experimenting with basic git stuff
