477502acd8851751fb4c5b019e88837a368003db
chriszq <cchhristopher@gmail.com>
2018-04-02 22:39:12 +0100

    making 2 changes and 1 add

--- SecondTestFile.java
+++ SecondTestFile.java
@@ -3,6 +3,7 @@ public class SecondTestFile
 	public static void main(String[] args)
 	{
 		System.out.println("second file");
+		System.out.println("adding an extra line here");
 	}
 }
 //experimenting with basic git stuff
--- TestA.java
+++ TestA.java
@@ -3,5 +3,6 @@ public class TestA
 	public static void main(String[] args)
 	{
 		System.out.println("testing git commands");
+		//adding a new comment here to see if it gets recorded as a change
 	}
 }
